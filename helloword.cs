300511ea1742aecca984db93c83314ee17803800
Manikha Meyyuliarti Nurozi <94760424+Manikhaa@users.noreply.github.com>
2022-09-20 10:36:22 +0700

    Create helloword.cs

---
+++ helloword.cs
@@ -0,0 +1,7 @@
+Console.WriteLine("Hello World!");
+Console.WriteLine("ini tugas pertama c# ku");
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code!");
+//halo ini comment 
