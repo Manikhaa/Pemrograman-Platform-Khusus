8ac7d7f1b99e85b385c9457b3977ca5b3f2f4b1f
Manikha Meyyuliarti Nurozi <94760424+Manikhaa@users.noreply.github.com>
2022-09-20 11:20:22 +0700

    Create boolliteral.cs

---
+++ boolliteral.cs
@@ -0,0 +1,14 @@
+Console.WriteLine(123);
+Console.WriteLine(12.30m);
+
+//string
+Console.WriteLine("true");
+//bool literal
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
