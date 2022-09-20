1585806dddb2fc69764961e1a67de775a4e781ec
Manikha Meyyuliarti Nurozi <94760424+Manikhaa@users.noreply.github.com>
2022-09-20 17:39:22 +0700

    Create Dataconversion.cs

---
+++ Dataconversion.cs
@@ -0,0 +1,16 @@
+Console.WriteLine("Simple addition and implicit data conversion\n");
+int firstnumber = 12;
+int secondnumber = 7;
+Console.WriteLine("Angka Pertama :" + firstnumber);
+Console.WriteLine("Angka Kedua :" +secondnumber);
+
+Console.WriteLine("Total :" +(firstnumber + secondnumber));
+Console.WriteLine("\n");
+
+//menghubungkan +, string dan int
+string Nama = "Manikha";
+int total = 8;
+Console.WriteLine("Minggu lalu " + Nama + " membeli " + total +" bola basket ");
+
+//step 3
+Console.WriteLine("Minggu lalu " + Nama + " membeli " + (8 + total) + " bola basket.");
